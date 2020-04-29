ed7bd8819f6bc89ef6f8ce02d4d05a87c7758f05
merrillrudd <merrillrudd@gmail.com>
2020-04-29 11:46:49 -0700

    code for running LIME

--- sim_new/simulations_runLIME.R
+++ sim_new/simulations_runLIME.R
@@ -41,9 +41,9 @@ ignore <- sapply(1:length(R_fun), function(x) source(file.path(R_path, R_fun[x])
 
 ## read life history info
 lh_info <- read.csv(file.path(proj_path, "life_history_info.csv")) %>%
-  mutate(ShortName = ifelse(Longevity == "Shorter" & Growth == "Slower", "shs",
-                            ifelse(Longevity == "Shorter" & Growth == "Faster", "shf",
-                                   ifelse(Longevity == "Longer" & Growth == "Slower", "los", "lof"))))
+  mutate(ShortName = ifelse(Longevity == "Shorter" & Growth == "Slower", "short_slow",
+                            ifelse(Longevity == "Shorter" & Growth == "Faster", "short_fast",
+                                   ifelse(Longevity == "Longer" & Growth == "Slower", "long_slow", "long_fast"))))
 
 L_a <- sapply(1:nrow(lh_info), function(x) lh_info[x,"Linf"]*(1 - exp(-lh_info[x,"k"]*(0:lh_info[x,"Amax"] - (-1)))))
 lh_info$L_at_Amin <- sapply(1:length(L_a), function(x) L_a[[x]][1])
@@ -86,15 +86,16 @@ true <- true_wide %>%
 
 #######################################
 ### ---- LIME -------###
-start <- Sys.time()
 
+##############################
+## Length only
 lime_dir <- file.path(sim_path, "LIME_results")
 dir.create(lime_dir, showWarnings = FALSE)
 
 data_grid <- unique(data %>% select(SigmaR, LifeHistory, L, Samp, F, Rich))
 sig_vec <- unique(data_grid$SigmaR)
 lh_vec <- unique(data_grid$LifeHistory)[1]
-lyears_vec <- unique(data_grid$L)
+lyears_vec <- c("L75","L20")
 samp_vec <- unique(data_grid$Samp)
 f_vec <- unique(data_grid$F)
 data_vec <- c("Catch_LC", "LC")
@@ -105,43 +106,64 @@ scen_grid <- expand.grid("LifeHistory" = lh_vec,
 						"Samp" = samp_vec,
 						"L" = lyears_vec,
 						"Data" = data_vec)
-scen_grid <- scen_grid[rev(order(scen_grid$LifeHistory)),]
+# scen_grid <- scen_grid[rev(order(scen_grid$LifeHistory)),]
 
-itervec <- 1
+itervec <- 1:50
 rewrite = FALSE
 
-ncores <- 5
-registerDoParallel(ncores)
-getParWorkers()    
+ncores <- 4
+cl <- makeCluster(ncores)
+registerDoParallel(cl)      
+
+start <- Sys.time()
 lime_res <- foreach(x=1:nrow(scen_grid), .packages=c('tidyverse')) %dopar%{
- 
-  devtools::load_all( path = lime_path)
+# lime_res <- lapply(1:nrow(scen_grid), function(x){
 
-  start <- Sys.time()
+  lh <- as.character(scen_grid[x,"LifeHistory"])
+  dir.create(file.path(lime_dir, lh), showWarnings = FALSE)
 
-  lh <- scen_grid[x,"LifeHistory"]
   f <- scen_grid[x,"F"]
+  dir.create(file.path(lime_dir, lh, f), showWarnings = FALSE)
+  
   sig <- scen_grid[x,"SigmaR"]
-  modpath <- file.path(sim_path, lh, f, sig, "LIME_EM")
-  dir.create(path, showWarnings = FALSE)
+  dir.create(file.path(lime_dir, lh, f, sig), showWarnings = FALSE)
 
-  lh_input <- lh_list[[lh]]
+  samp_dir <- file.path(lime_dir, lh, f, sig, scen_grid[x,"Samp"])
+  dir.create(samp_dir, showWarnings = FALSE)
+
+  lh_input <- lh_list[[which(names(lh_list) == lh)]]
   lh_input$SigmaF <- 0.1
   if(sig == "LowSigmaR") lh_input$SigmaR <- 0.4
   if(sig == "HighSigmaR") lh_input$SigmaR <- 0.8
   
   byIter <- lapply(1:length(itervec), function(ii){
-  	ipath <- file.path(modpath, itervec[ii])
+  # byIter <- foreach(ii=1:length(itervec), .export = c("itervec","scen_grid","rewrite"), .packages=c('tidyverse')) %dopar%{
+  	
+ 
+	lime_path <- "C:\\merrill\\LIME"
+	devtools::load_all(path = lime_path)
+
+
+  	ipath <- file.path(samp_dir, itervec[ii])
   	dir.create(ipath, showWarnings = FALSE)
-  	if(rewrite==TRUE & file.exists(file.path(ipath,"LIME_output.rds"))) unlink(file.path(ipath, "LIME_output.rds"), TRUE)
+
+ 	 lyr_dir <- file.path(ipath, scen_grid[x,"L"])
+ 	 dir.create(lyr_dir, showWarnings = FALSE)
+ 	 lyears <- as.numeric(str_split(as.character(scen_grid[x,"L"]),"L")[[1]][2])
+
+ 	 	em_path <- file.path(lyr_dir, scen_grid[x,"Data"])
+	  	if(rewrite == TRUE) unlink(em_path, TRUE)
+ 	 	dir.create(em_path, showWarnings = FALSE)
+
+ 	 	if(file.exists(file.path(em_path, "LIME_output.rds")) == FALSE){
 
 			idata <- data %>% 
-				filter(iteration == itervec[ii]) %>%
-				filter(SigmaR == sig) %>%
-				filter(LifeHistory == lh) %>%
-				filter(L == scen_grid[x,"L"]) %>%
-				filter(Samp == scen_grid[x,"Samp"]) %>%
-				filter(F == f)=
+				dplyr::filter(iteration == itervec[ii]) %>%
+				dplyr::filter(SigmaR == sig) %>%
+				dplyr::filter(LifeHistory == lh) %>%
+				dplyr::filter(L == "L75") %>%
+				dplyr::filter(Samp == scen_grid[x,"Samp"]) %>%
+				dplyr::filter(F == f)
 
 			years <- idata$year
 			catch <- matrix(idata$catch,nrow = 1)
@@ -155,60 +177,93 @@ lime_res <- foreach(x=1:nrow(scen_grid), .packages=c('tidyverse')) %dopar%{
 			colnames(lf_mat) <- bins
 			rownames(lf_mat) <- years
 
+			## check number of years of length data for scenario
+			nodata_vec <- 1:(100 - lyears)
+			lf_mat[nodata_vec,] <- 0
+
 			data_list_toUse <- list("years"=years, "LF"=lf_mat, "C_ft"=catch)
 			inputs <- create_inputs(lh=lh_input, input_data=data_list_toUse)
 
 			first_yr <- as.numeric(which(rowSums(lf_mat) > 0)[1])
-			agemat <- lh_input$AgeMax
-			first_rdev <- max(first_yr - agemat,1)
+			agemax <- lh_input$AgeMax
+			first_rdev <- max(first_yr - agemax,1)
 
 			est_rdev_t_inp <- rep(1,length(years))
-			if(first_rdev < 1) est_rdev_t_inp[1:(first_rdev-1)] <- 0
+			if(first_rdev > 1) est_rdev_t_inp[1:(first_rdev-1)] <- 0
 
-			start <- Sys.time()
-			out <- run_LIME(modpath=NULL,
+			if(scen_grid[x,"Data"] == "LC"){
+				data_avail <- "LC"
+				C_type <- 0
+				SigRpen <- 1
+				Fpen <- 1
+			}
+			if(scen_grid[x,"Data"] == "Catch_LC"){
+				data_avail <- "Catch_LC"
+				C_type <- 2
+				SigRpen <- 0
+				Fpen <- 0
+			}
+
+			# start <- Sys.time()
+			out <- run_LIME(modpath=em_path,
 							input=inputs,
-							data_avail="Catch_LC",
+							data_avail=data_avail,
 							newtonsteps=3,
-							C_type=2,
-							LFdist=1,
+							C_type=C_type,
+							LFdist=0,
 							rewrite=rewrite,
 							derive_quants=TRUE, 
 							est_rdev_t = est_rdev_t_inp,
-							SigRpen = 0,
-							Fpen = 0)
-			end_mod <- Sys.time() - start
+							SigRpen = SigRpen,
+							Fpen = SigRpen)
+			# end_mod <- Sys.time() - start
 
-
-			t1 <- true %>% filter(iteration == 1) %>% filter(lifehistory == as.character(lh))
-			x1 <- t1 %>% filter(variable == "F")
+			# t1 <- true %>% filter(iteration == 1) %>% filter(lifehistory == as.character(lh))
+			# x1 <- t1 %>% filter(variable == "F")
 
 			if(all(is.null(out$df))){
-				write("model NA", file.path(modpath, itervec[ii], paste0("LIME_modelNA.txt")))
+				write("model NA", file.path(em_path,paste0("LIME_modelNA.txt")))
 			}
 			if(all(is.null(out$df))==FALSE){
 				gradient <- out$opt$max_gradient <= 0.001
 				pdHess <- out$Sdreport$pdHess
 				if(gradient==FALSE){
-					write("highgradient", file.path(modpath, itervec[ii], paste0( "LIME_highgradient.txt")))
+					write("highgradient", file.path(em_path, paste0( "LIME_highgradient.txt")))
 				}
 				if(pdHess==FALSE){
-					write("Hessian not positive definite", file.path(modpath, itervec[ii], paste0("LIME_pdHessFALSE.txt")))
+					write("Hessian not positive definite", file.path(em_path, paste0("LIME_pdHessFALSE.txt")))
 				}
 				## save results if converged
-				if(gradient == TRUE & pdHess == TRUE) saveRDS(out, file.path(modpath, itervec[ii], "LIME_output.rds"))
+				if(gradient == TRUE & pdHess == TRUE) saveRDS(out, file.path(em_path, "LIME_output.rds"))
 			}
-
+		}
   })
-  
-
-  return(out)
-}
+}#)
+end <- Sys.time() - start
 stopCluster(cl)
 
-lime_res <- do.call(rbind, lime_res)
+re <- lapply(1:nrow(scen_grid), function(x){
+	byIter <- lapply(1:length(itervec), function(y){
+		res1 <- readRDS(file.path(lime_dir, "short_slow", "F1", "LowSigmaR", "N200", 1, "L20", "LC", "LIME_output.rds"))
+		res2 <- readRDS(file.path(lime_dir, "short_slow", "F1", "LowSigmaR", "N200", 1, "L75", "LC", "LIME_output.rds"))
+		t1 <- true %>% filter(lifehistory == "short_slow") %>% filter(iteration == 1) %>% filter(Fscen == "F1") %>% filter(SigmaR == "LowSigmaR")
+
+	})
+})
 
 
 
-true <- readRDS(file.path(case_path, scen_vec[x], 20, "True.rds"))
-res <- readRDS(file.path(case_path, scen_vec[x], 20, "LIME_output.rds"))
+plot(t1$F, ylim=c(0,1))
+
+lines(res1$Report$F_ft[1,])
+lines(res2$Report$F_ft[1,], col = "blue")
+
+plot(t1$Depletion, ylim = c(0,1))
+lines(res1$Report$D_t)
+lines(res2$Report$D_t, col = "blue")
+
+plot(t1$Recruit, ylim = c(0, max(t1$Recruit)))
+lines(res1$Report$R_t)
+lines(res2$Report$R_t, col = "blue")
+
+res$Report$sigma_R
